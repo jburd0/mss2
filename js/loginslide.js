93d3c961dcb017679798034db7b67dfae83173cb
johnburd <jburd@silverburd.com>
2015-05-20 09:42:26 -0500

    Autohide loginslider when mouse leaves for 3 seconds.

--- js/loginslide.js
+++ js/loginslide.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 	var slideHeight = $(".login-slide").outerHeight();
 	$(".login-slide").css({"top": -slideHeight});
-	$("[role='reveal']").mouseover(
-		function(){
-			$(".login-slide").stop().animate({"top": "0"});
-		}
-	);
-	$("[role='escape']").click(
-		function() {
+	$("[role='reveal']").mouseover(function(){
+		$(".login-slide").stop().animate({"top": "0"});
+	});
+	$("[role='escape']").click(function() {
+		$(".login-slide").stop().animate({"top": -slideHeight});
+		return false;
+	});
+	var hideTimer = null;
+	$(".login-slide").bind('mouseleave', function() {
+		hideTimer = setTimeout(function() {
 			$(".login-slide").stop().animate({"top": -slideHeight});
-			return false;
-		}
-	);
+		}, 3000);
+	});
 });
 
